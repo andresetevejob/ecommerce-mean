edd7f91bf10c3886c8ce850b5cd68f3759fe202d
KOUAME KONAN KHAN Andre <a_kouame@sesame.infotel.com>
2019-10-15 16:37:28 +0200

    feature inscription

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+
+  constructor(private location:Location,
+    private userService:UserService,
+    private router:Router) { 
+      this.user = new User();
+    }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.userService.signUp(this.user).subscribe(u=>{
+      this.router.navigate(['/login']);
+    },e=>{});
   }
 
 }
